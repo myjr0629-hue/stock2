e23bbc3dad7b2bd0d1a8b56b557fdd14bc23c2b8
Stock2 Dev <dev@stock2.local>
2026-03-02 22:48:23 +0900

    fix: update phone number in shared Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -46,7 +46,7 @@ export function Footer() {
                             상호명: 은훈마스터 &nbsp;|&nbsp; 대표자: 김지영 &nbsp;|&nbsp; 사업자등록번호: 473-15-01443 &nbsp;|&nbsp; 통신판매업 신고번호: 제 2024-경기안산-2779호
                         </p>
                         <p>
-                            사업장 소재지: 경기도 안산시 단원구 선부로 286, 3층 302호 G4 &nbsp;|&nbsp; 고객센터: 070-000-000 &nbsp;|&nbsp; 이메일: contact@signumhq.com
+                            사업장 소재지: 경기도 안산시 단원구 선부로 286, 3층 302호 G4 &nbsp;|&nbsp; 고객센터: 070-8027-2912 &nbsp;|&nbsp; 이메일: contact@signumhq.com
                         </p>
                     </div>
                 )}
